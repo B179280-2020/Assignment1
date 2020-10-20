e06891fb67295477ef155fc331bcc00fd8a35f3e
B179280-2020 <email_not_provided>
2020-10-20 15:22:20 +0100

    Adding unzip feature to the scripts

--- fastqc.sh
+++ fastqc.sh
@@ -3,3 +3,8 @@ for fastqfile in /localdisk/data/BPSM/Assignment1/fastq/*.fq.gz
 do
   fastqc -o ~/Assignment1/fqc_results $fastqfile
 done
+for zipfile in ~/Assignment1/fqc_results/*.zip
+do
+unzip -o $zipfile -d ~/Assignment1/fqc_results
+done
+
