b246912ba530fd782bde676be4a5aa16d20cbf61
B179280-2020 <email_not_provided>
2020-10-20 22:09:34 +0100

    updated version--can assess the number and quality of the sequence

--- fastqc.sh
+++ fastqc.sh
@@ -7,4 +7,7 @@ for zipfile in ~/Assignment1/fqc_results/*.zip
 do
 unzip -o $zipfile -d ~/Assignment1/fqc_results
 done
+cat ~/Assignment1/fqc_results/*fastqc/fastqc_data.txt | grep -E "Filename|Total Sequences" > ~/Assignment1/fqc_results/fastqc_summaries.txt 
+
+cat ~/Assignment1/fqc_results/*fastqc/summary.txt >> ~/Assignment1/fqc_results/fastqc_summaries.txt
 
