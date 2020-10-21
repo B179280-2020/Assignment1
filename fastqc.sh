15f9edf3ba3825d71063995ad87c84264ce139f9
B179280-2020 <email_not_provided>
2020-10-21 22:00:30 +0100

    established genome index, producing .sam file

--- fastqc.sh
+++ fastqc.sh
@@ -18,4 +18,26 @@ done
 cat ./results/*fastqc/fastqc_data.txt | grep -E "Filename|Total Sequences" > ./results/fastqc_summaries.txt 
 
 cat ./results/*fastqc/summary.txt >> ./results/fastqc_summaries.txt
+#cope reference genome sequence data
+cp -r /localdisk/data/BPSM/Assignment1/Tbb_genome ./ref
+#copy the original fastq directory to current path
+cp -r /localdisk/data/BPSM/Assignment1/fastq ./data
+#unzip
+gunzip ./data/*.gz
+#change directory
+cd ./ref/
+#unzip
+gunzip Tb927_genome.fasta.gz
+#establish index
+bowtie2-build Tb927_genome.fasta Tbb
+#reture to last directory
+cd ../
+#alignment, producing .sam file
+for i in 216 218 219 220 221 222
+do
+bowtie2 -x ./ref/Tbb -1 ./data/$i"_L8_1.fq" -2 ./data/$i"_L8_2.fq" -S ./data/"$i".sam
+done
+
+
+
 
