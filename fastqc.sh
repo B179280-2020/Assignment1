aa26e78a5f3be555f255678aff5ff7de2ef307c4
B179280-2020 <email_not_provided>
2020-10-21 14:22:54 +0100

    changed the path that the scripts will be run

--- fastqc.sh
+++ fastqc.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #Executing the fastqc with iteration, then  putting the results into a new directory for storage
-
+mkdir ./results
 for fastqfile in /localdisk/data/BPSM/Assignment1/fastq/*.fq.gz
 do
-  fastqc -o ~/Assignment1/fqc_results $fastqfile
+  fastqc -o ./results $fastqfile
 done
 
 #unzip each result file with .zip
 
-for zipfile in ~/Assignment1/fqc_results/*.zip
+for zipfile in ./results/*.zip
 do
-unzip -o $zipfile -d ~/Assignment1/fqc_results
+unzip -o $zipfile -d ./results
 done
 
 #assess the numbers and quality of sequence data based on fastqc output, putting the information into a file for summary
 
-cat ~/Assignment1/fqc_results/*fastqc/fastqc_data.txt | grep -E "Filename|Total Sequences" > ~/Assignment1/fqc_results/fastqc_summaries.txt 
+cat ./results/*fastqc/fastqc_data.txt | grep -E "Filename|Total Sequences" > ./results/fastqc_summaries.txt 
 
-cat ~/Assignment1/fqc_results/*fastqc/summary.txt >> ~/Assignment1/fqc_results/fastqc_summaries.txt
+cat ./results/*fastqc/summary.txt >> ./results/fastqc_summaries.txt
 
